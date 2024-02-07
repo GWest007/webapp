7ab13b81b0a515d8a9bbff65bb2bc4164e7de934
GWest007 <anji.west@yahoo.com>
2024-02-07 01:15:22 -0800

    Add files via upload

---
+++ website1/src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Subscription from "./Subscription";
+import AboutUs from "./AboutUs";
+import Footer from "./Footer";
+import Home from "./Home";
+
+export {
+  Navbar,
+  Subscription,
+  Business,
+  AboutUs,
+  Footer,
+  Home,
+};
